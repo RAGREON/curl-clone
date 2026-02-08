2af279cd117ef4a0694b779b933783a1a6693cd4
RAGREON <ragreon809@gmail.com>
2026-02-08 17:07:53 +0545

    (feat): use Client class

--- main.cpp
+++ main.cpp
@@ -1,97 +1,22 @@
-#pragma comment(lib, "Ws2_32.lib")
-
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <iostream>
-#include <stdlib.h>
-
-#define LOG_ERROR(info) \
-  std::cout << "LINE: " << __LINE__ << " | "; \
-  std::cerr << info << " " << WSAGetLastError() << std::endl;
-
-#define BUFFER_SIZE 1024
+#include "client.h"
 
 int main(int argc, char** argv) {
+  std::string ip = argv[1];
+  int port = std::stoi(argv[2]);
 
-  std::cout << "Hello World!!" << std::endl;
+  Client* _client = new Client();
 
-  /*
-    WSA Startup 
-  */
-  WSADATA wsaData = { };
-  int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-  if (iResult != 0) {
-    LOG_ERROR("WSA Startup failed");
-    return 1;
-  }
-
-  /*
-    Socket that will connect to the servers
-    Client Socket
-
-    Address Family = INET / IPv4
-    Type = TCP connection
-  */
-  SOCKET connectSocket;
-  connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (connectSocket == INVALID_SOCKET) {
-    LOG_ERROR("Connect Socket creation failed");
-  }
+  _client->setServerAddress(ip, port);
   
-  /*
-    Server socket address 
-  */
-  sockaddr_in serverAddress;
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
-  serverAddress.sin_port = htons(std::stoi(argv[2]));
+  _client->openConnection();
 
-  iResult = connect(connectSocket, (SOCKADDR*) &serverAddress, sizeof(serverAddress));
-  if (iResult == SOCKET_ERROR) {
-    LOG_ERROR("Connect failed");
-    iResult = closesocket(connectSocket);
+  std::string request =
+    "GET / HTTP/1.1\r\n"
+    "Host: 127.0.0.1:8070\r\n"
+    "Connection: close\r\n\r\n";
+  _client->sendRequest(request);
 
-    if (iResult == SOCKET_ERROR)
-      LOG_ERROR("Failed to close socket");
+  _client->recvResponse();
 
-    WSACleanup();
-    return 1;
-  }
-  
-  std::cout << "successfully connected to server!!" << std::endl;
-
-  if (iResult == SOCKET_ERROR) {
-    LOG_ERROR("Failed to close socket");
-    WSACleanup();
-    return 1;
-  }
-
-  std::string request = "GET / HTTP/1.1\r\nHost: 127.0.0.1:5500\r\n\r\n"; 
-
-  iResult = send(connectSocket, request.c_str(), (int)request.length(), 0);
-  if (iResult == SOCKET_ERROR) {
-    LOG_ERROR("Failed to send request");
-    WSACleanup();
-    return 1;
-  }
-
-  char recvBuf[BUFFER_SIZE];
-
-  do {
-    iResult = recv(connectSocket, recvBuf, BUFFER_SIZE, 0);
-    if (iResult > 0) {
-      std::cout << recvBuf;
-    }
-    else if (iResult == 0) {
-      std::cout << "Server closed connection\n";
-    }
-    else {
-      LOG_ERROR("Receive failed");
-    }
-  } while (iResult > 0);
-
-  closesocket(connectSocket);
-
-  WSACleanup();
   return 0;
 }
